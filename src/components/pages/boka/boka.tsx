caa6bb95f9e9fcc287596dc12f337933da029a50
[juliafumiko]”git config --global user.name juliafumiko]” <juliafumikodahl@gmail.com>
2020-08-28 10:39:04 +0200

    booking

--- src/components/pages/boka/boka.tsx
+++ src/components/pages/boka/boka.tsx
@@ -1,17 +1,14 @@
 import React from 'react'
 
-function boka() {
-    return (
-        <div>
-            <form>
-                <h3>Make reservation</h3>
-                <input type="name" placeholder="first name"></input>
-                <input type="name" placeholder="last name"></input>
-                <input type="email" placeholder="email"></input>
-                <button type="submit"> Place reservation </button>
-                <link rel="stylesheet" href=""/> Show my reservation</form>
-        </div>
-    )
+function Boka() {
+    return <form>
+    <h3>Make reservation</h3>
+    <input type="name" placeholder="first name"></input>
+    <input type="name" placeholder="last name"></input>
+    <input type="email" placeholder="email"></input>
+    <button type="submit"> Place reservation </button> <br></br>
+    <link rel="stylesheet" href=""/> View my reservation
+  </form>
 }
 
-export default boka
+export default Boka
