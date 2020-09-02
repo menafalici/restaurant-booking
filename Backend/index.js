eb6762f52e82f84446d6a0098ad4d1f9d15460e8
marcusgardh <58325557+marcusgardh@users.noreply.github.com>
2020-09-02 11:52:08 +0200

    get och post på admin-sidan

---
+++ Backend/index.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+
+const config = require("./database/config");;
+const Reservation = require("./models/Reservation");
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+// app.use(cors({
+//     origin: 'http://localhost:3000'
+//   }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', async function (req, res) {
+    let reservationArray = [];
+    let reservations  = await Reservation.find({
+        // name: "Skor"
+    }, "name date time");
+
+    reservationArray = reservations;
+  
+    console.log(reservationArray);
+
+    res.send(reservationArray);
+});
+
+app.post("/", async (req, res) => {
+
+    let reservation = req.body;
+
+    console.log(reservation)
+
+    let newReservation = new Reservation({
+        name : reservation.name,
+        date : reservation.date,
+        time : reservation.time
+    })
+
+    console.log(newReservation)
+
+    await newReservation.save((error, success) => {
+        if (error) {
+            res.send(error._message);
+        }
+        else {
+            res.redirect("/");
+        }
+    });
+})
+
+mongoose
+.connect(config.databaseURL, config.options)
+.then(() => {
+    app.listen(config.port);
+}).catch((e) => {
+    console.log(e);
+})
---
+++ Backend/models/Reservation.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+let reservationSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        required: true
+    },
+    time: {
+        type: Number,
+        required: true
+    }
+})
+
+let Reservation = mongoose.model("reservation", reservationSchema);
+
+module.exports = Reservation;
